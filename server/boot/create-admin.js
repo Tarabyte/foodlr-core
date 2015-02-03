da0f34a546b83795b2f61c83e7f6f33b4d04b9d5
Yury Tarabanko <tarabanko@mirapolis.ru>
2015-02-03 16:48:25 +0300

    save users and tokens to mongo

--- server/boot/create-admin.js
+++ server/boot/create-admin.js
@@ -1,14 +1,31 @@
 module.exports = function(app, next) {
   console.log('Creating default admin.');
-  app.models.User.create({
-    username: 'admin',
-    email: 'noreply@foodlr.ru',
-    password: 'pkjt,exrf'
-  }, function(err, user) {
+  app.models.User.findOne({username: 'admin'}, function(err, admin){
     if(err) {
-      console.log('Error on creating user.', err);
+      console.log('Error on fetching admin user %s', err.message);
+      next(err);
     }
-    console.log(user);
-    next();
+    else {
+      if(admin) {
+        console.log('Admin user exists.');
+        next();
+      }
+      else {
+        app.models.User.create({
+          username: 'admin',
+          email: 'noreply@foodlr.ru',
+          password: 'pkjt,exrf'
+        }, function(err, user) {
+          if(err) {
+            console.log('Error on creating user %s.', err.message);
+          }
+          else {
+            console.log(user);
+          }
+          next();
+        });
+      }
+    }
   });
+
 };
